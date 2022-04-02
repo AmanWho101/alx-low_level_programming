89eba6e5cc109287c6b8a8350a7bed4c3024cee4
AmanWho101 <Amleaz2005@gmail.com>
2022-04-02 20:29:32 +0300

    all

--- 0x09-static_libraries/0-strcat.c
+++ 0x09-static_libraries/0-strcat.c
@@ -1,22 +1,33 @@
-#include "main.h"
+#include "holberton.h"
+
 /**
- * _strcat - appends src to the dest string
- * @dest: string to append by src
- * @src: string to append to dest
- *
- * Return: address of dest
+ * *_strcat - function commute srtings
+ * @dest: param pointer to a char
+ * @src: param pointer to a char
+ * Return: return value of dest
  */
+
 char *_strcat(char *dest, char *src)
 {
-int i, j;
+	int i;
+	int j;
 
-i = j = 0;
-while (*(dest + i))
-i++;
-while ((*(dest + i) = *(src + j)))
-{
-i++;
-j++;
+	i = 0;
+	j = 0;
+
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+
+	while (src[j] != '\0')
+	{
+		dest[i] = src[j];
+		j++;
+		i++;
+	}
+
+	dest[i] = '\0';
+	return (dest);
 }
-return (dest);
-}
+
